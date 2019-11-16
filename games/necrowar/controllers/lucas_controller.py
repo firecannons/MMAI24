76a8a1eb0d3f72530b4c36f1ced54778f7240c7a
firecannons <lawf48@mst.edu>
2019-11-16 16:10:16 -0600

    Visualizer working!

--- games/necrowar/controllers/lucas_controller.py
+++ games/necrowar/controllers/lucas_controller.py
@@ -31,52 +31,46 @@ if DRAW == True:
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     unit_font = pygame.font.SysFont("comicsansms", 20)
 
-def draw(game):
-    global Game
-    Game = game
-    if DRAW == True:
-        draw_map()
-
-def getGame():
-    global Game
-    return Game
-
-def draw_map():
-    TileDimension = float(WINDOW_WIDTH) / MAP_WIDTH
-    for x in range(MAP_WIDTH):
-        for y in range(MAP_HEIGHT):
-            TileColor = PATH_COLOR
-            if getGame().tiles[x + y * MAP_WIDTH].is_river == True:
-                TileColor = WATER_COLOR
-            if getGame().tiles[x + y * MAP_WIDTH].is_path == True:
-                TileColor = PATH_COLOR
-            if getGame().tiles[x + y * MAP_WIDTH].is_grass == True:
-                TileColor = GRASS_COLOR
-            if getGame().tiles[x + y * MAP_WIDTH].is_tower == True:
-                TileColor = GRASS_COLOR
-            if getGame().tiles[x + y * MAP_WIDTH].is_gold_mine == True or getGame().tiles[x + y * MAP_WIDTH].is_island_gold_mine == True:
-                TileColor = GOLD_COLOR
-            if getGame().tiles[x + y * MAP_WIDTH].is_castle == True:
-                TileColor = CASTLE_COLOR
-            if getGame().tiles[x + y * MAP_WIDTH].is_worker_spawn == True:
-                TileColor = WORKER_SPAWN_COLOR
-            if getGame().tiles[x + y * MAP_WIDTH].is_unit_spawn == True:
-                TileColor = UNIT_SPAWN_COLOR
-            if getGame().tiles[x + y * MAP_WIDTH].unit != None:
-                if getGame().tiles[x + y * MAP_WIDTH].unit.owner == getPlayer():
-                    TileColor = BLUE_UNIT_COLOR
-                else:
-                    TileColor = RED_UNIT_COLOR
-                unit_text = unit_font.render(getGame().tiles[x + y * MAP_WIDTH].unit.job.title[0], True, UNIT_TEXT_COLOR)
-                screen.blit(text, (TileDimension * x, TileDimension * y))
-            TileRect = pygame.Rect(TileDimension * x, TileDimension * y, TileDimension * x + TileDimension, TileDimension * y + TileDimension)
-            pygame.draw.rect(screen, TileColor, TileRect)
-    pygame.display.flip()
-
 class LucasController(BaseController):
     def __init__(self, logger: logging.Logger, game: Game, player: Player):
         super().__init__(logger, game, player)
 
     def run_turn(self):
-        draw(self._game)
+        self.draw()
         self.logger.info(f'Hello from Lucas!')
+    
+    def draw(self):
+        if DRAW == True:
+            self.draw_map()
+
+    def draw_map(self):
+        TileDimension = float(WINDOW_WIDTH) / MAP_WIDTH
+        for x in range(MAP_WIDTH):
+            for y in range(MAP_HEIGHT):
+                TileColor = PATH_COLOR
+                if self.game.tiles[x + y * MAP_WIDTH].is_river == True:
+                    TileColor = WATER_COLOR
+                if self.game.tiles[x + y * MAP_WIDTH].is_path == True:
+                    TileColor = PATH_COLOR
+                if self.game.tiles[x + y * MAP_WIDTH].is_grass == True:
+                    TileColor = GRASS_COLOR
+                if self.game.tiles[x + y * MAP_WIDTH].is_tower == True:
+                    TileColor = GRASS_COLOR
+                if self.game.tiles[x + y * MAP_WIDTH].is_gold_mine == True or self.game.tiles[x + y * MAP_WIDTH].is_island_gold_mine == True:
+                    TileColor = GOLD_COLOR
+                if self.game.tiles[x + y * MAP_WIDTH].is_castle == True:
+                    TileColor = CASTLE_COLOR
+                if self.game.tiles[x + y * MAP_WIDTH].is_worker_spawn == True:
+                    TileColor = WORKER_SPAWN_COLOR
+                if self.game.tiles[x + y * MAP_WIDTH].is_unit_spawn == True:
+                    TileColor = UNIT_SPAWN_COLOR
+                if self.game.tiles[x + y * MAP_WIDTH].unit != None:
+                    if self.game.tiles[x + y * MAP_WIDTH].unit.owner == self.player:
+                        TileColor = BLUE_UNIT_COLOR
+                    else:
+                        TileColor = RED_UNIT_COLOR
+                    unit_text = unit_font.render(self.game.tiles[x + y * MAP_WIDTH].unit.job.title[0], True, UNIT_TEXT_COLOR)
+                    screen.blit(unit_text, (TileDimension * x, TileDimension * y))
+                TileRect = pygame.Rect(TileDimension * x, TileDimension * y, TileDimension * x + TileDimension, TileDimension * y + TileDimension)
+                pygame.draw.rect(screen, TileColor, TileRect)
+        pygame.display.flip()
